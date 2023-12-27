92422d6fe8715f607bb3a002537ba79c30afdf1c
Connor <cmallonisme@gmail.com>
2023-12-27 17:27:49 +0000

    Add Profile.html and Profile.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from templates.workouts import *
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from templates.workouts import get_todays_workouts
 app = Flask(__name__)
 app.secret_key = 'secret_key' 
 
@@ -9,15 +9,13 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # Here you would validate the username and password
-        # For simplicity, we'll assume any user is valid
         session['username'] = username
         return redirect(url_for('home'))
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-   # session.pop('username', None)  # Remove 'username' from session
+    session.pop('username', None)  # Remove 'username' from session
     return redirect(url_for('login'))
 
 @app.route('/')
@@ -35,5 +33,11 @@ def todays_workouts_api():
     return jsonify(todays_workouts)
 
 
+def profile():
+    if 'username' in session:
+        return render_template('profile.html', username=session['username'])
+    else:
+        return redirect(url_for('login'))
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- templates/Profile.py
+++ templates/Profile.py
@@ -1,5 +1,45 @@
-from flask import Blueprint, Flask, jsonify
-import datetime
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    # Check if username is in the session
+    if 'username' not in session:
+        # Redirect to the login page if the user is not logged in
+        return redirect(url_for('login'))
+
+    username = session['username']
+
+    # Fetch user details from database based on username
+    user = user.query.filter_by(username=username).first()
+
+    if request.method == 'POST':
+        # Extract data from form submission
+        name = request.form.get('name')
+        age = request.form.get('age')
+        password = request.form.get('changepassword')  # Ensure this matches your form's input name
+
+        # Update user details and password if a new one is provided
+        if user:
+            user.name = name
+            user.age = age
+
+            if password:  # Update the password only if it's provided
+                user.password_hash = generate_password_hash(password)
+
+            # Save changes to database
+            # db.session.commit()
+
+            flash('Your profile was updated successfully!', 'success')
+
+        # Redirect back to the profile page or a confirmation page as needed
+        return redirect(url_for('profile'))
+
+    # Render the profile template, passing in user details
+    return render_template('profile.html', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
